f6f56188b7712acb493bd7d4ae625c331675e3b6
Rebekka Moyson <rebekka.moyson@ml6.eu>
2021-03-18 09:16:01 +0100

    add extra tests

--- models/marts/user_behaviour/commits_enhanced_basic.sql
+++ models/marts/user_behaviour/commits_enhanced_basic.sql
@@ -1,13 +1,15 @@
+{{
+    config(
+        materialized='ephemeral'
+    )
+}}
+
 WITH commits_with_timezone as (
 SELECT 
 commits.*,
 mapping.timezone,
 DATETIME(TIMESTAMP_SECONDS(commits.committer.time_sec), mapping.timezone) as committer_local_datetime
-{% if target.name  != 'prd' %}
 FROM {{ source('github', 'sample_commits')}} as commits
-{% else %}
-FROM {{ source('github', 'commits')}} as commits
-{% endif %}
 
 LEFT JOIN {{ ref('mapping_time_offset_zone')}} as mapping
 ON commits.committer.tz_offset = mapping.tz_offset
